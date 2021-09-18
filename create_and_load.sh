003b9113aea5a9fbb1f3b9151926ac5415d9c0cb
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2021-09-18 05:07:33 -0500

    Update create_and_load.sh

--- create_and_load.sh
+++ create_and_load.sh
@@ -23,27 +23,17 @@ echo "Schema created."
 
 echo "Loading data..."
 
-# Load airlines into flights.airlines
+# Load airlines into travel.airlines
 ${mariadb} -e "LOAD DATA LOCAL INFILE 'data/airlines.csv' INTO TABLE airlines FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n'" travel
 
 echo "- airlines.csv loaded into travel.airlines"
 
-# Load airports into flights.airlines
+# Load airports into travel.airlines
 ${mariadb} -e "LOAD DATA LOCAL INFILE 'data/airports.csv' INTO TABLE airports FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n'" travel
 echo "- airports.csv loaded into travel.airports"
 
+## Load flights data into travel.flights
 echo "- travel.flights"
-#!/bin/bash
-# Check for argument, if so use as wildcard for file load match, otherwise load everything
-filematch="*"
-if [ $# -eq 1 ]
-then
-  filematch="*$1*"
-fi
-# Load the specified files under the data directory with the file pattern match
-for f in flight_data/$filematch.csv; do
-  echo "  - $f"
-  #cpimport "travel" "flights" "flight_data/${f}" -s "," -E '"'
-  ${mariadb} -e "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE flights FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n'" travel
-done
+  ${mariadb} -e "LOAD DATA LOCAL INFILE 'data/flights.csv' INTO TABLE flights FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n'" travel
+
 echo "Done!"
